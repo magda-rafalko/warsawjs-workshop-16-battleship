211b235cfc46de0aa17b706341049f4d7d306ce8
Magda Rafałko <magda.filipkowskaa@gmail.com>
2018-01-21 16:00:14 +0100

    add controller

--- game.js
+++ game.js
@@ -13,13 +13,12 @@ class ViewComponent {
 }
 
 class GameCell extends ViewComponent{
-    constructor() {
+    constructor(handleCellClick, row, column) {
         super();
         this._state = 'unknown';
         this._element = document.createElement('td');
-        const self = this;
-        this._element.addEventListener('click', function _handleClick() {
-            self.setState('miss');
+        this._element.addEventListener('click', function() {
+            handleCellClick(row, column);
         });
     }
     setState(state) {
@@ -29,26 +28,51 @@ class GameCell extends ViewComponent{
        this._state = state;
        this._element.className = 'cell_' + state;
     }
-
 }
 
 class GameBoard extends ViewComponent {
-    constructor() {
+    constructor(handleCellClick) {
         super();
         this._element = document.createElement('table');
+        this._cells = {};
 
-        for (let i = 0; i < 10; i++) {
+        for (let rowIndex = 0; rowIndex < 10; rowIndex++) {
             const row = document.createElement('tr');
 
-            for (let i = 0; i < 10; i++) {
-                const cell = new GameCell();
+            for (let columnIndex = 0; columnIndex < 10; columnIndex++) {
+                const cell = new GameCell(handleCellClick, rowIndex, columnIndex);
                 row.appendChild(cell.getElement());
+                const coordinatesText = rowIndex + 'x' +columnIndex;
+                this._cells[coordinatesText] = cell;
             }
             this._element.appendChild(row);
         }
     }
+    setStateAt(row, column, state) {
+        const coordinatesText = row + 'x' + column;
+        this._cells[coordinatesText].setState(state);
+    }
+}
+
+class GameController {
+    constructor(boardView) {
+        this._boardView = boardView;
+    }
+    handleCellClick(row, column) {
+        this._boardView.setStateAt(row, column, 'miss');
+    }
 }
 
 const gameElement = document.getElementById('game');
-const gameBoard = new GameBoard();
-gameElement.appendChild(gameBoard.getElement());
+let board;
+let controller;
+
+function handleCellClick(row, column) {
+    controller.handleCellClick(row, column);
+}
+
+board = new GameBoard(handleCellClick);
+controller = new GameController(board);
+
+gameElement.appendChild(board.getElement());
+board.setStateAt(0, 0, 'miss');
